f3128d92e1ca63f385b38ac4ca6f360fd580610f
Pragatheesh <pragatheeshsp26@gmail.com>
2026-05-08 00:32:36 +0530

    Updated gitignore

--- database/history_connection.py
+++ database/history_connection.py
@@ -1,14 +1,18 @@
+import os
 import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def get_history_connection():
 
     conn = mysql.connector.connect(
-        host="localhost",
-        port=3306,
-        user="root",
-        password="Praga08@@",
-        database="ai_sql_history"
+        host=os.getenv("MYSQLHOST"),
+        port=os.getenv("MYSQLPORT"),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE")
     )
 
     return conn
